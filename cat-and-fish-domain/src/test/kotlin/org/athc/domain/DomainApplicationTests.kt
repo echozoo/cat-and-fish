9dff5337b50452651a4ed7ea9b38d83efad26d6f
dujf <dujf54732@gmail.com>
2021-05-19 18:59:11 +0800

    feat: README.md

--- cat-and-fish-domain/src/test/kotlin/org/athc/domain/DomainApplicationTests.kt
+++ cat-and-fish-domain/src/test/kotlin/org/athc/domain/DomainApplicationTests.kt
@@ -1,9 +1,7 @@
 package org.athc.domain
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class DomainApplicationTests {
 
 	@Test
